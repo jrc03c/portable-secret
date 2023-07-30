85720698111771ec0b7c02b5f0dc350ec6490fdb
jrc03c <jrc03c@pm.me>
2023-07-29 20:10:37 -0500

    ...

--- res/js/src/encrypt.js
+++ res/js/src/encrypt.js
@@ -1,10 +1,42 @@
+const { encrypt } = require("@jrc03c/js-crypto-helpers")
+
 const template = /* html */ `
   <div>
-    This is the ENCRYPT view.
+    <form @submit.prevent="encrypt">
+      <div class="control">
+        <label class="radio">
+          <input
+            name="input-type"
+            type="radio"
+            v-model="inputType"
+            value="text">
+          Text
+        </label>
+
+        <label class="radio">
+          <input
+            name="input-type"
+            type="radio"
+            v-model="inputType"
+            value="file">
+          File
+        </label>
+      </div>
+    </form>
   </div>
 `
 
 module.exports = {
   name: "x-encrypt",
   template,
+
+  data() {
+    return {
+      inputType: "text",
+    }
+  },
+
+  methods: {
+    encrypt() {},
+  },
 }
